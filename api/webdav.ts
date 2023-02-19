a840e0f8d7c70565f67f05bc98e90574a5f9e2df
BjChacha <bjchacha@outlook.com>
2023-02-20 07:35:39 +0800

    begin to write api

--- api/webdav.ts
+++ api/webdav.ts
@@ -1,3 +1,13 @@
+interface WebdavData {
+	url: string;
+	account: string;
+	password: string;
+}
+
 export function test() {
-	console.log('this is a webdav function');
+	console.log("this is a webdav function");
+}
+
+export async function webdavVerify(info: WebdavData): Promise<boolean> {
+	return true;
 }
