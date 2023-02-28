c7e5fe6404cb461c6c8f19973940a9e31adfaf1e
=BjChacha <bjchacha@outlook.com>
2023-02-28 21:06:54 +0800

    doing: webdav connection

--- api/webdav.ts
+++ api/webdav.ts
@@ -1,11 +1,24 @@
-interface WebdavData {
-	url: string;
-	account: string;
-	password: string;
-}
+import { request } from "obsidian";
+import { WebdavData } from "../interface";
 
-export function test() {
-	console.log("this is a webdav function");
+export async function test() {
+	console.log("this is a webdav test function");
+
+	const prop = {
+		url: "https://dav.jianguoyun.com/dav/",
+		method: "OPTIONS",
+		// 403：这里差验证
+		headers: {
+			method: "OPTIONS",
+			mode: "cors",
+			credentials: "same-origin",
+			referrerPolicy: "no-referrer",
+		},
+	};
+
+	const response = await request(prop);
+
+	return response;
 }
 
 export async function webdavVerify(info: WebdavData): Promise<boolean> {
---
+++ interface.ts
@@ -0,0 +1,5 @@
+export interface WebdavData {
+	url: string;
+	account: string;
+	password: string;
+}
